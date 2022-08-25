1cd650cf4029fc0324a50d769cd69096b89f9341
SH-NicholasPage <npage@southhills.edu>
2022-08-24 20:07:31 -0400

    Edit for comments

--- CppMakeEqual/CppMakeEqual/Source.cpp
+++ CppMakeEqual/CppMakeEqual/Source.cpp
@@ -1,4 +1,9 @@
 /*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+/*
 	Takes two integer arrays as inputs
 	Return how many operations it takes to make the two arrays equal
 	Return -1 if the two arrays cannot be made equal or constraints are not met
